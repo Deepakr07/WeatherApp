b4c609543c105dfeb93e23defd3ca5860c85b90b
deepak <deepakrajesh567@gmail.com>
2023-03-14 21:32:51 +0530

    Integrated api services

--- lib/Screens/landingScreen.dart
+++ lib/Screens/landingScreen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/Services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+const apiKey = 'd508af4235b5c0a0bf7f8bda8ff8cff2';
 
 class LandingPage extends StatefulWidget {
   //const LandingPage({Key? key}) : super(key: key);
@@ -8,6 +12,9 @@ class LandingPage extends StatefulWidget {
 }
 
 class _LandingPageState extends State<LandingPage> {
+  double? latitude;
+  double? longitude;
+  @override
   void initState() {
     super.initState();
     getLocation();
@@ -16,8 +23,28 @@ class _LandingPageState extends State<LandingPage> {
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
+    latitude = location.latitude;
+    longitude = location.longitude;
     print(location.latitude);
     print(location.longitude);
+    getData();
+  }
+
+  void getData() async {
+    http.Response response = await http.get(Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&'
+        'lon=$longitude&appid=$apiKey'));
+    if (response.statusCode == 200) {
+      String data = response.body;
+      var temperature = jsonDecode(data)['main']['temp'];
+      var description = jsonDecode(data)['weather'][0]['description'];
+      var place = jsonDecode(data)['sys']['country'];
+      print(place);
+      print(description);
+      print(temperature);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
